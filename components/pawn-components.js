2fb43c7a9c05f1c1c38fd966e9201fccd9bcf68b
Mahendra Kolhe <mkolhe23@gmail.com>
2023-05-26 10:08:22 +0000

    chess js and css

--- components/pawn-components.js
+++
@@ -1,8 +0,0 @@
-//create pawn component
-const pawnCompo = "<div class='pawnCompo'><img src='./pieces/black/pawn.png' alt='chess-pawn-piece'/></div>";
-
-filesNameArray.forEach((i) => {
-    const id = i + 2;
-    const element = document.getElementById(id);
-    
-});
